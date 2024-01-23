bf4cc2a089c7dc89d9176478b1b06bbfcc1a643b
luispino32 <luispino32@hotmail.com>
2024-01-23 12:53:56 -0500

    :construction: (utils): [ADD] Array of Countries

---
+++ src/utils/countries.js
@@ -0,0 +1,19 @@
+
+const countries = [
+    {name: 'Argentina'},
+    {name: 'Bolivia'},
+    {name: 'Brasil'},
+    {name: 'Chile'},
+    {name: 'Colombia'},
+    {name: 'Ecuador'},
+    {name: 'Guayana Francesa'},
+    {name: 'Guyana'},
+    {name: 'Islas Malvinas'},
+    {name: 'Paraguay'},
+    {name: 'Perú'},
+    {name: 'Surinam'},
+    {name: 'Uruguay'},
+    {name: 'Venezuela'}
+];
+
+module.exports = countries;
