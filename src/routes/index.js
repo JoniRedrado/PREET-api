aace7ea2ff80b65d51cd362fa4b7e8db21602d40
Jonathan Redrado <jonathan.redrado98@gmail.com>
2024-01-23 00:59:25 -0300

    Project Structure

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 // Importar todos los routers;
-
+const hotelsRouter = require("./hotelsRouter")
+const countriesRouter = require("./countriesRouter")
 const router = Router();
 
 // Configurar los routers
-//router.use("/pokemon", pokemonRouter)
+router.use("/hotels", hotelsRouter)
+router.use("/countries", countriesRouter)
 
 
 module.exports = router;
