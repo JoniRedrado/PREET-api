9f2b9c2b824eac1ee98831c99c22eba9552f1595
luispino32 <luispino32@hotmail.com>
2024-01-23 17:45:53 -0500

    :construction: (.env, db.js): [UPDATE] Change default value

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/preet`,
+   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/PREET`,
    {
       logging: false,
       native: false,
