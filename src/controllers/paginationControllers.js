2df1e777982586d19ed9f7830af8e219346f697d
luispino32 <luispino32@hotmail.com>
2024-01-26 18:19:19 -0500

    :construction: [UPDATE]: optimization getHotels route

--- src/controllers/paginationControllers.js
+++ src/controllers/paginationControllers.js
@@ -1,18 +1,20 @@
 const { Hotel } = require('../../db.js');
 
 const getItems = async (req, res) => {
-    const { page = 1, size = 2 } = req.query;
+    const { page, size } = req.query;
 
     const options = {
         limit: Number(size),
-        offset: (page - 1) * Number(size)
+        offset: (Number(page) - 1) * Number(size)
     };
-        const { count, rows } = await Hotel.findAndCountAll(options);
-        res.json({
-            status: "success",
-            total: count,
-            Hotel: rows,
-        });
+
+    const { count, rows } = await Hotel.findAndCountAll(options);
+
+    res.json({
+        status: "success",
+        total: count,
+        Hotel: rows,
+    });
 
 }; 
 
