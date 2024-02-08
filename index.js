12961cf520e45b277075be8671f2b26b72cd1f74
Jonathan Redrado <jonathan.redrado98@gmail.com>
2024-02-07 22:01:18 -0300

    Deploy config

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { PORT } = process.env
 
 const findOrCreateData = require('./src/utils/dataDefaultPostgres.js');
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   findOrCreateData();
 
   server.listen(PORT, () => {
