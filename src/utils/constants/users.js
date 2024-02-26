3bbb2814555a7c452c789d6e5f952404d688170e
RafaelDGonzalezN <david.gnzlz.224@gmail.com>
2024-02-26 14:11:53 -0500

    :sparkles: Profile_image

--- src/utils/constants/users.js
+++ src/utils/constants/users.js
@@ -45,6 +45,28 @@ const lastNames = [
     'Muñoz',
     'Blanco'
 ];
+const imageUrls = [
+    'https://res.cloudinary.com/dcposqieu/image/upload/f_auto,q_auto/v1/ImageUsers/swv4oyp72koikhjkwyoq',
+    'https://res.cloudinary.com/dcposqieu/image/upload/f_auto,q_auto/v1/ImageUsers/ds1l81ac0iymt3v2erhg',
+    'https://res.cloudinary.com/dcposqieu/image/upload/f_auto,q_auto/v1/ImageUsers/rxt0zrrf784nvq5iwhc6',
+    "https://res.cloudinary.com/dcposqieu/image/upload/f_auto,q_auto/v1/ImageUsers/iywooni90qpaxbure1jw",
+    "https://res.cloudinary.com/dcposqieu/image/upload/f_auto,q_auto/v1/ImageUsers/tm6i8cv7lh4prq4soowz",
+    "https://res.cloudinary.com/dcposqieu/image/upload/f_auto,q_auto/v1/ImageUsers/dkygnajvb26tyyw8ihzs",
+    "https://res.cloudinary.com/dcposqieu/image/upload/f_auto,q_auto/v1/ImageUsers/xznrbnq42qdriuelcpog",
+    "https://res.cloudinary.com/dcposqieu/image/upload/f_auto,q_auto/v1/ImageUsers/nwfmmti9efmxdkxeztrw",
+    "https://res.cloudinary.com/dcposqieu/image/upload/f_auto,q_auto/v1/ImageUsers/grdifnq6fvq7s25pluqz",
+    "https://res.cloudinary.com/dcposqieu/image/upload/f_auto,q_auto/v1/ImageUsers/bvwxdju6beznjcbpawhi",
+    "https://res.cloudinary.com/dcposqieu/image/upload/f_auto,q_auto/v1/ImageUsers/zf80m0wdpxmsbw8asoi2",
+    "https://res.cloudinary.com/dcposqieu/image/upload/f_auto,q_auto/v1/ImageUsers/lnwed8uzzaeypbxrjp7m",
+    "https://res.cloudinary.com/dcposqieu/image/upload/f_auto,q_auto/v1/ImageUsers/h3pzlvdoqznlu7llune5",
+    "https://res.cloudinary.com/dcposqieu/image/upload/f_auto,q_auto/v1/ImageUsers/j7ydigjcmd7raen7edko",
+    "https://res.cloudinary.com/dcposqieu/image/upload/f_auto,q_auto/v1/ImageUsers/f2fmb9hg7xkkcscr3hje",
+    "https://res.cloudinary.com/dcposqieu/image/upload/f_auto,q_auto/v1/ImageUsers/g9dmyeq3gxvajs8hqyyd",
+    "https://res.cloudinary.com/dcposqieu/image/upload/f_auto,q_auto/v1/ImageUsers/rlzkxuqul9kveacmuk3b",
+    "https://res.cloudinary.com/dcposqieu/image/upload/f_auto,q_auto/v1/ImageUsers/onvivcfnespmdivqbq9w",
+    "https://res.cloudinary.com/dcposqieu/image/upload/f_auto,q_auto/v1/ImageUsers/bgezebn1v4nq42ncwuxl",
+    "https://res.cloudinary.com/dcposqieu/image/upload/f_auto,q_auto/v1/ImageUsers/eogi2ytf4avvrhtia0j7"
+];
 
 const createLastNameAndEmail = (name, users) => {
     let email;
@@ -74,6 +96,7 @@ const getRandomUser = () => {
                 email: data.email,
                 nationality: countries[Math.floor(Math.random()*countries.length)].name,
                 password: `${name}_${data.last_name}_123`,
+                profile_picture: imageUrls[Math.floor(Math.random() * imageUrls.length)]
             }
         });
 
